3c2c11422fbab5899a4b46098058471975c17a9c
lys1313013 <lys1313013@gmail.com>
2025-04-06 18:55:49 +0800

    refactor: 根据数据字典类型获取值集增加返回参数说明

--- apps/web-antd/src/api/core/common.ts
+++ apps/web-antd/src/api/core/common.ts
@@ -6,7 +6,22 @@ import { requestClient } from '#/api/request';
  * @param dictType 数据字典类型
  * @returns 枚举值列表
  */
-export async function getByDictType(dictType: number) {
+// 假设已经定义了 SysDictTypeEntity 和 SysDictDataEntity 类型
+interface SysDictTypeEntity {
+  dictName: string;
+}
+
+interface SysDictDataEntity {
+  dictLabel: string;
+  dictValue: string;
+}
+
+interface ResponseData {
+  sysDictTypeEntity: SysDictTypeEntity;
+  dictDetailList: SysDictDataEntity[];
+}
+
+export async function getByDictType(dictType: string): Promise<ResponseData> {
   return await requestClient.get('/sysDictType/getByDictType', {
     params: { dictType },
   });
