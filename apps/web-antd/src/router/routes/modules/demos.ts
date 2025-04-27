c997853566a1fbf2d11589c5647985f5e64547a5
lys1313013 <lys1313013@gmail.com>
2025-04-27 22:46:11 +0800

    feat!: 移除原项目演示代码

--- apps/web-antd/src/router/routes/modules/demos.ts
+++ apps/web-antd/src/router/routes/modules/demos.ts
@@ -1,26 +1,16 @@
 import type { RouteRecordRaw } from 'vue-router';
 
-import { $t } from '#/locales';
-
 const routes: RouteRecordRaw[] = [
   {
     meta: {
       icon: 'ic:baseline-view-in-ar',
       keepAlive: true,
       order: 1000,
-      title: $t('demos.title'),
+      title: '个人中心',
     },
     name: 'Demos',
     path: '/demos',
     children: [
-      {
-        meta: {
-          title: $t('demos.antd'),
-        },
-        name: 'AntDesignDemos',
-        path: '/demos/ant-design',
-        component: () => import('#/views/demos/antd/index.vue'),
-      },
       {
         meta: {
           title: '字典管理',
