3048fdca54c9b24eebfa1e93022f195a4507b322
lys1313013 <lys1313013@gmail.com>
2025-11-29 14:21:48 +0800

    feat(time-tracker): 更新默认时间段分类颜色配置

--- apps/web-antd/src/views/demos/time-tracker/config.ts
+++ apps/web-antd/src/views/demos/time-tracker/config.ts
@@ -2,60 +2,60 @@ import type { TimeTrackerConfig, TimeSlotCategory } from './types';
 
 // 默认时间段分类
 export const defaultCategories: TimeSlotCategory[] = [
-   {
+  {
     id: 'project',
     name: '项目',
-    color: '#eb2f96',
+    color: '#722ed1', // purple-6
   },
   {
     id: 'study',
     name: '学习',
-    color: '#52c41a',
+    color: '#52c41a', // green-6
   },
   {
     id: 'work',
     name: '工作',
-    color: '#1890ff',
+    color: '#1677ff', // blue-6
   },
   {
     id: 'rest',
     name: '休息',
-    color: '#faad14',
+    color: '#faad14', // gold-6
   },
   {
     id: 'entertainment',
     name: '娱乐',
-    color: '#722ed1',
+    color: '#eb2f96', // magenta-6
   },
   {
     id: 'exercise',
     name: '运动',
-    color: '#f50',
+    color: '#fa541c', // volcano-6
   },
   {
     id: 'eat',
     name: '吃饭',
-    color: '#fa541c',
+    color: '#fa8c16', // orange-6
   },
   {
     id: 'wash',
     name: '洗漱',
-    color: '#13c2c2',
+    color: '#13c2c2', // cyan-6
   },
   {
     id: 'commuting',
     name: '交通',
-    color: '#eb2f96',
+    color: '#2f54eb', // geekblue-6
   },
   {
     id: 'finance-investment',
     name: '理财',
-    color: '#fa8c16',
+    color: '#f5222d', // red-6
   },
   {
     id: 'other',
     name: '其他',
-    color: '#d9d9d9',
+    color: '#bfbfbf', // grey-6
   }
 ];
 
